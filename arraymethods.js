7da9263ea7111a05cb63e0343e406a9e5539569d
ShameemaAfzal <shameemakamil97@gmail.com>
2024-04-24 15:45:53 +0530

    Array methods Task and Eg

---
+++ arraymethods.js
@@ -0,0 +1,20 @@
+const fruits = ["banana","orange","Mango","Kiwi","Litchi"]
+//   Length
+    console.log(fruits.length); 
+    // toString
+    console.log(fruits.toString());
+//   join
+    console.log(fruits.join('*'));
+//   Push
+    fruits.push('pomegranate');
+    console.log(fruits);
+//   Pop
+    console.log(fruits.pop());
+//   Shift
+    fruits.shift();
+    console.log(fruits);
+//   Splice
+    fruits.splice(1,1,"grapes","guava")
+    console.log(fruits);
+//   Slice
+    console.log(fruits.slice(2));
---
+++ eg.js
@@ -0,0 +1,4 @@
+let arr = [2,4,6,8,10]				
+let newarr = arr.filter(item => item%4 == 0)
+console.log(newarr);   
+console.log(arr);
